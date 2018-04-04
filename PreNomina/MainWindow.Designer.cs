7bd1be302e1da4fa54742220d173ee76c8d39c4b
RodrigoDiazC <mirodi81@gmail.com>
2018-04-04 15:04:53 -0500

    Added tab control to display other views

--- PreNomina/MainWindow.Designer.cs
+++ PreNomina/MainWindow.Designer.cs
@@ -65,12 +65,17 @@
             this.rb_TrabajoF = new System.Windows.Forms.RadioButton();
             this.rb_Asistencia = new System.Windows.Forms.RadioButton();
             this.cb_NoReg = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
             this.toolStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // openFileDialog
@@ -89,7 +94,7 @@
             this.dataGrid.AllowUserToAddRows = false;
             this.dataGrid.AllowUserToResizeRows = false;
             this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGrid.Location = new System.Drawing.Point(12, 46);
+            this.dataGrid.Location = new System.Drawing.Point(12, 74);
             this.dataGrid.Name = "dataGrid";
             this.dataGrid.ReadOnly = true;
             this.dataGrid.RowHeadersVisible = false;
@@ -103,7 +108,7 @@
             this.dataGrid1.AllowUserToDeleteRows = false;
             this.dataGrid1.AllowUserToResizeRows = false;
             this.dataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGrid1.Location = new System.Drawing.Point(296, 46);
+            this.dataGrid1.Location = new System.Drawing.Point(6, 40);
             this.dataGrid1.Name = "dataGrid1";
             this.dataGrid1.ReadOnly = true;
             this.dataGrid1.RowHeadersVisible = false;
@@ -118,7 +123,7 @@
             this.tsb_Exportar});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(984, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(988, 25);
             this.toolStrip1.TabIndex = 3;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -146,7 +151,7 @@
             // lb_Empleados
             // 
             this.lb_Empleados.AutoSize = true;
-            this.lb_Empleados.Location = new System.Drawing.Point(12, 25);
+            this.lb_Empleados.Location = new System.Drawing.Point(12, 50);
             this.lb_Empleados.Name = "lb_Empleados";
             this.lb_Empleados.Size = new System.Drawing.Size(98, 13);
             this.lb_Empleados.TabIndex = 5;
@@ -155,7 +160,7 @@
             // lb_Horarios
             // 
             this.lb_Horarios.AutoSize = true;
-            this.lb_Horarios.Location = new System.Drawing.Point(293, 25);
+            this.lb_Horarios.Location = new System.Drawing.Point(3, 19);
             this.lb_Horarios.Name = "lb_Horarios";
             this.lb_Horarios.Size = new System.Drawing.Size(97, 13);
             this.lb_Horarios.TabIndex = 5;
@@ -218,7 +223,7 @@
             // 
             this.cb_Retardos.AutoSize = true;
             this.cb_Retardos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cb_Retardos.Location = new System.Drawing.Point(624, 22);
+            this.cb_Retardos.Location = new System.Drawing.Point(334, 16);
             this.cb_Retardos.Name = "cb_Retardos";
             this.cb_Retardos.Size = new System.Drawing.Size(76, 19);
             this.cb_Retardos.TabIndex = 10;
@@ -230,7 +235,7 @@
             // 
             this.cb_Anticipo.AutoSize = true;
             this.cb_Anticipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cb_Anticipo.Location = new System.Drawing.Point(807, 22);
+            this.cb_Anticipo.Location = new System.Drawing.Point(517, 16);
             this.cb_Anticipo.Name = "cb_Anticipo";
             this.cb_Anticipo.Size = new System.Drawing.Size(75, 19);
             this.cb_Anticipo.TabIndex = 10;
@@ -242,7 +247,7 @@
             // 
             this.cb_Excedente.AutoSize = true;
             this.cb_Excedente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cb_Excedente.Location = new System.Drawing.Point(887, 22);
+            this.cb_Excedente.Location = new System.Drawing.Point(597, 16);
             this.cb_Excedente.Name = "cb_Excedente";
             this.cb_Excedente.Size = new System.Drawing.Size(84, 19);
             this.cb_Excedente.TabIndex = 10;
@@ -261,9 +266,9 @@
             this.groupBox1.Controls.Add(this.cb_Puntualidad);
             this.groupBox1.Controls.Add(this.cb_Asistencia);
             this.groupBox1.Controls.Add(this.cb_Desempeno);
-            this.groupBox1.Location = new System.Drawing.Point(296, 179);
+            this.groupBox1.Location = new System.Drawing.Point(6, 173);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(296, 174);
+            this.groupBox1.Size = new System.Drawing.Size(296, 164);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Info General";
@@ -313,7 +318,7 @@
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.lb_Sta);
             this.groupBox2.Controls.Add(this.tb_Acceso);
-            this.groupBox2.Location = new System.Drawing.Point(598, 179);
+            this.groupBox2.Location = new System.Drawing.Point(308, 173);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(374, 77);
             this.groupBox2.TabIndex = 13;
@@ -372,9 +377,9 @@
             this.groupBox4.Controls.Add(this.rb_Permiso);
             this.groupBox4.Controls.Add(this.rb_TrabajoF);
             this.groupBox4.Controls.Add(this.rb_Asistencia);
-            this.groupBox4.Location = new System.Drawing.Point(598, 260);
+            this.groupBox4.Location = new System.Drawing.Point(308, 254);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(284, 93);
+            this.groupBox4.Size = new System.Drawing.Size(284, 83);
             this.groupBox4.TabIndex = 14;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Info de Día";
@@ -450,7 +455,7 @@
             // 
             this.cb_NoReg.AutoSize = true;
             this.cb_NoReg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cb_NoReg.Location = new System.Drawing.Point(705, 22);
+            this.cb_NoReg.Location = new System.Drawing.Point(415, 16);
             this.cb_NoReg.Name = "cb_NoReg";
             this.cb_NoReg.Size = new System.Drawing.Size(97, 19);
             this.cb_NoReg.TabIndex = 15;
@@ -458,22 +463,53 @@
             this.cb_NoReg.UseVisualStyleBackColor = true;
             this.cb_NoReg.CheckedChanged += new System.EventHandler(this.cb_NoReg_CheckedChanged);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(288, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(698, 369);
+            this.tabControl1.TabIndex = 16;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.dataGrid1);
+            this.tabPage1.Controls.Add(this.cb_NoReg);
+            this.tabPage1.Controls.Add(this.lb_Horarios);
+            this.tabPage1.Controls.Add(this.groupBox4);
+            this.tabPage1.Controls.Add(this.cb_Retardos);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.cb_Anticipo);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.cb_Excedente);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(690, 343);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(202, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 365);
-            this.Controls.Add(this.cb_NoReg);
-            this.Controls.Add(this.groupBox4);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.cb_Excedente);
-            this.Controls.Add(this.cb_Anticipo);
-            this.Controls.Add(this.cb_Retardos);
-            this.Controls.Add(this.lb_Horarios);
+            this.ClientSize = new System.Drawing.Size(988, 388);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.lb_Empleados);
             this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.dataGrid1);
             this.Controls.Add(this.dataGrid);
             this.Controls.Add(this.bt_Abrir);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -489,6 +525,9 @@
             this.groupBox2.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -532,6 +571,9 @@
         private System.Windows.Forms.RadioButton rb_TrabajoF;
         private System.Windows.Forms.ToolStripButton tsb_Exportar;
         private System.Windows.Forms.CheckBox cb_NoReg;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
